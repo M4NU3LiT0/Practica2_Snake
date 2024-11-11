682b82bd414b29e5cfce5a51dda7cf3277e3c59f
M4NU3LiT0 <105329911+M4NU3LiT0@users.noreply.github.com>
2024-11-11 16:41:38 -0600

    Update Gordita snake.c
    Aquí agrandamos la serpiente a que sea de 4x2 como lo indica la practica.

--- snake.c
+++ snake.c
@@ -8,14 +8,14 @@
 // pointer a las leds
 volatile unsigned int *led_base = LED_MATRIX_0_BASE;
 volatile unsigned int *snake[L];  //array de la serpiente
-int snakeLenght = 4;
+int snakeLenght = 8;
 
 void bordes() { 
     // horizontales
     for (int i = 0; i < W; i++) {
         *(led_base + i) = 0xFF0000;               //  borded e arriba
-        *(led_base + (W * (H - 1)) + i) = 0xFF0000;
-    } // borde de abajo
+        *(led_base + (W * (H - 1)) + i) = 0xFF0000;// borde de abajo
+    }
     //verticales
     for (int i = 0; i < H; i++) {
         *(led_base + (W * i)) = 0xFF0000;         // izquierda
@@ -30,11 +30,22 @@ void clear() {
 }
 
 void spawnSnake() {
-    snake[0] = led_base + W +2; //cabeza
-    snake[1] = led_base + W + 4; //cuerpo
+    //primera fila:
+    snake[0] = led_base + W +2;
+    snake[1] = led_base + W +3;
+    snake[2] = led_base + W +4;
+    snake[3] = led_base + W +5;
+     //segunda fila:
+     snake[4] = led_base + (2 * W) + 2;
+    snake[5] = led_base + (2 * W) + 3;
+    snake[6] = led_base + (2 * W) + 4;
+    snake[7] = led_base + (2 * W) + 5;
+     
+     
     //color - verde
-    *snake[0] = 0x00FF00;
-    *snake[1] = 0x00FF00;
+    for (int i=0; i<snakeLenght; i++) {
+        *snake[i] = 0x00FF00;
+    }
 }
 
 int main() {
