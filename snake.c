22159eae0937a210a8d92624c9dffd13bf3da3eb
M4NU3LiT0 <105329911+M4NU3LiT0@users.noreply.github.com>
2024-11-11 16:38:57 -0600

    Update snake.c
    Aquí se muestra un avance de como creamos la snake.

--- snake.c
+++ snake.c
@@ -1,17 +1,21 @@
 #include "ripes_system.h"
 #include <stdio.h>
 
-#define W LED_MATRIX_0_WIDTH
-#define H LED_MATRIX_0_HEIGHT
+#define W LED_MATRIX_0_WIDTH //width
+#define H LED_MATRIX_0_HEIGHT //height 
+#define L (H*W)
 
 // pointer a las leds
 volatile unsigned int *led_base = LED_MATRIX_0_BASE;
+volatile unsigned int *snake[L];  //array de la serpiente
+int snakeLenght = 4;
 
 void bordes() { 
     // horizontales
     for (int i = 0; i < W; i++) {
         *(led_base + i) = 0xFF0000;               //  borded e arriba
-        *(led_base + (W * (H - 1)) + i) = 0xFF0000; // borde de abajo
+        *(led_base + (W * (H - 1)) + i) = 0xFF0000;
+    } // borde de abajo
     //verticales
     for (int i = 0; i < H; i++) {
         *(led_base + (W * i)) = 0xFF0000;         // izquierda
@@ -25,9 +29,18 @@ void clear() {
     }
 }
 
+void spawnSnake() {
+    snake[0] = led_base + W +2; //cabeza
+    snake[1] = led_base + W + 4; //cuerpo
+    //color - verde
+    *snake[0] = 0x00FF00;
+    *snake[1] = 0x00FF00;
+}
+
 int main() {
     clear();
     bordes();
+    spawnSnake();
     while(1) {
         ///
     }
