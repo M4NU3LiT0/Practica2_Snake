d102a37d0a33f68d8cc83d00df5397dc64ea347b
Retr0 <jose.rodriguezv@iteso.mx>
2024-11-11 20:19:27 -0500

    corregí error en la linea 38, iteré mal y se habia hecho otra vez de una linea

--- snake.c
+++ snake.c
@@ -6,24 +6,21 @@
 
 volatile unsigned int *led_base = LED_MATRIX_0_BASE;
 volatile unsigned int *snake[L];  // array de la serpiente
-int snakeLenght = 8;
+int snakeLenght = 8; 
 int pos_x = 1;
 int pos_y = 1;
 
 void bordes() { 
-    // horizontales
     for (int i = 0; i < W; i++) {
-        *(led_base + i) = 0xFF0000; //borded e arriba
-        *(led_base + (W * (H - 1)) + i) = 0xFF0000; //borde de abajo
+        *(led_base + i) = 0xFF0000; //arriba
+        *(led_base + (W * (H - 1)) + i) = 0xFF0000; //abajo
     }
-    // verticales
     for (int i = 0; i < H; i++) {
         *(led_base + (W * i)) = 0xFF0000; //izquierda
         *((led_base + (W * i)) + (W - 1)) = 0xFF0000; //derecha
     }
 }
 
-//funcion para limpiar los pmixeles
 void clear() {
     for (int i = 0; i < (W * H); i++) {
         *(led_base + i) = 0x00;
@@ -32,17 +29,17 @@ void clear() {
 
 void spawnSnake() {
     // Primera fila (4) --- snake[0] = led_base + W + 2;
-    int limFila = snakeLenght/2;
-    for (int i=0; i<limFila; i++) {
-        snake[i] = led_base + W + (i+2);
+    int limit = snakeLenght / 2; 
+    for (int i = 0; i < limit; i++) {
+        snake[i] = led_base + W + (i + 2);
     }
-    
     // Segunda fila (4) --- snake[4] = led_base + (2 * W) + 2;
-     for (int i=0; i<limFila; i++) {
-        snake[i] = led_base + (2 * W) + (i+2);
+    for (int i = 0; i < limit; i++) {
+        snake[i + limit] = led_base + (2 * W) + (i + 2);//corregido xd
     }
     
-    // color - verde
+    
+    // Color - verde
     for (int i = 0; i < snakeLenght; i++) {
         *snake[i] = 0x00FF00;
     }
@@ -53,19 +50,19 @@ void moveDerecha() {
     *snake[0] = 0x000000;
     *snake[snakeLenght/2] = 0x000000;
     
-    // Mover primera fila
+    // Mover primera fila:
     for (int i = 0; i < (snakeLenght/2) - 1; i++) {
         snake[i] = snake[i + 1];
     }
     snake[(snakeLenght/2) - 1] = snake[(snakeLenght/2) - 2] + 1;
     
-    // Mover segunda fila, empezamos desde donde la dejamos
+    // Mover segunda fila:
     for (int i = snakeLenght/2; i < snakeLenght - 1; i++) {
         snake[i] = snake[i + 1];
     }
     snake[snakeLenght - 1] = snake[snakeLenght - 2] + 1;
     
-    // repintar
+    //repintamos
     for (int i = 0; i < snakeLenght; i++) {
         *snake[i] = 0x00FF00;
     }
@@ -82,7 +79,7 @@ int main() {
     spawnSnake();
     while (1) {
         moveDerecha();
-        delay(5); //cuando agarre una manzana podemos reducir esta wea para que vaya mas rapido
+        delay(5);
     }
     return 0;
 }
