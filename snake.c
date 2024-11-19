a544cf25a748fda9f42699a73af08aff8b9c6d45
M4NU3LiT0 <105329911+M4NU3LiT0@users.noreply.github.com>
2024-11-19 09:52:14 -0600

    se mueve horizontalmente de forma normal en 4x2, pero al moverse de manera vertical se convierte en una línea de 4 leds.

--- snake.c
+++ snake.c
@@ -18,34 +18,52 @@ unsigned short max_x = 33;
 unsigned short max_y = 23;
 
 
-// para guardar el historial de posiciones}
+// Snake coordinates arrays (dos columnas para el ancho)
 #define SNAKE_LENGTH 4
-unsigned int snake_x[SNAKE_LENGTH];  // coordenadas x/ segmento
-unsigned int snake_y[SNAKE_LENGTH];  // coordenadas y / segmento
+unsigned int snake_x[SNAKE_LENGTH][2];  // [segmento][columna]
+unsigned int snake_y[SNAKE_LENGTH][2];  // [segmento][columna]
 
-// Punteros a la cabeza y cola
-unsigned int *head_x;
-unsigned int *head_y;
-unsigned int *tail_x;
-unsigned int *tail_y;
+// Punteros a la cabeza (2 pixeles)
+unsigned int *head_x_left;
+unsigned int *head_y_left;
+unsigned int *head_x_right;
+unsigned int *head_y_right;
+
+// Punteros a la cola (2 pixeles)
+unsigned int *tail_x_left;
+unsigned int *tail_y_left;
+unsigned int *tail_x_right;
+unsigned int *tail_y_right;
 
 //colors
 unsigned int snake_color = 0x0000FF00;  // Verde
+
 
 void main() {
     clear();
     
     // Inicializar la serpiente en posición horizontal
     for(int i = 0; i < SNAKE_LENGTH; i++) {
-        snake_x[i] = 17 + i;  // Comienza en x=17
-        snake_y[i] = 12;      // Misma y para todos
+        // Columna izquierda
+        snake_x[i][0] = 17 + i;
+        snake_y[i][0] = 12;
+        // Columna derecha
+        snake_x[i][1] = 17 + i;
+        snake_y[i][1] = 13;  // Un pixel arriba para el ancho
     }
     
-    // Inicializar pointers
-    head_x = &snake_x[SNAKE_LENGTH-1];  // Último elemento (más a la derecha)
-    head_y = &snake_y[SNAKE_LENGTH-1];
-    tail_x = &snake_x[0];                // Primer elemento (más a la izquierda)
-    tail_y = &snake_y[0];
+    // Inicializar punteros
+    // Cabeza (último segmento)
+    head_x_left = &snake_x[SNAKE_LENGTH-1][0];
+    head_y_left = &snake_y[SNAKE_LENGTH-1][0];
+    head_x_right = &snake_x[SNAKE_LENGTH-1][1];
+    head_y_right = &snake_y[SNAKE_LENGTH-1][1];
+    
+    // Cola (primer segmento)
+    tail_x_left = &snake_x[0][0];
+    tail_y_left = &snake_y[0][0];
+    tail_x_right = &snake_x[0][1];
+    tail_y_right = &snake_y[0][1];
     
     // Dibujar serpiente inicial
     spawn_snake();
@@ -72,42 +90,53 @@ void main() {
 
 void movimiento(unsigned int mov) {
     // Guardar la posición antigua de la cola
-    unsigned int old_tail_x = *tail_x;
-    unsigned int old_tail_y = *tail_y;
+    unsigned int old_tail_x_left = *tail_x_left;
+    unsigned int old_tail_y_left = *tail_y_left;
+    unsigned int old_tail_x_right = *tail_x_right;
+    unsigned int old_tail_y_right = *tail_y_right;
     
     // Mover cada segmento a la posición del siguiente
     for(int i = 0; i < SNAKE_LENGTH-1; i++) {
-        snake_x[i] = snake_x[i+1];
-        snake_y[i] = snake_y[i+1];
+        snake_x[i][0] = snake_x[i+1][0];
+        snake_y[i][0] = snake_y[i+1][0];
+        snake_x[i][1] = snake_x[i+1][1];
+        snake_y[i][1] = snake_y[i+1][1];
     }
     
     // Mover la cabeza según la dirección
     switch(mov) {
         case 1://arriba
-            (*head_y)++;
+            (*head_y_left)++;
+            (*head_y_right)++;
             break;
         case 2://abajo
-            (*head_y)--;
+            (*head_y_left)--;
+            (*head_y_right)--;
             break;
         case 3://izq
-            (*head_x)--;
+            (*head_x_left)--;
+            (*head_x_right)--;
             break;
         case 4://derecha
-            (*head_x)++;
+            (*head_x_left)++;
+            (*head_x_right)++;
             break;
     }
     
     // Borrar la cola antigua
-    set_pixel(old_tail_x, old_tail_y, 0);
+    set_pixel(old_tail_x_left, old_tail_y_left, 0);
+    set_pixel(old_tail_x_right, old_tail_y_right, 0);
     
     // Dibujar la nueva cabeza
-    set_pixel(*head_x, *head_y, snake_color);
+    set_pixel(*head_x_left, *head_y_left, snake_color);
+    set_pixel(*head_x_right, *head_y_right, snake_color);
 }
 
 void spawn_snake() {
     // Dibujar cada segmento de la serpiente
     for(int i = 0; i < SNAKE_LENGTH; i++) {
-        set_pixel(snake_x[i], snake_y[i], snake_color);
+        set_pixel(snake_x[i][0], snake_y[i][0], snake_color);  // Columna izquierda
+        set_pixel(snake_x[i][1], snake_y[i][1], snake_color);  // Columna derecha
     }
 }
 
